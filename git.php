48f70686ffd2ca4716b8b474103620b470dec187
1337h4x0r <1337h4x0r@gmx.de>
2022-09-07 11:54:32 +0200

    start implementing password passing logic

--- git.php
+++ git.php
@@ -2,7 +2,40 @@
 
 declare(strict_types=1);
 
-function git_pull():bool {
+function git_pull(string &$password):bool {
+    $pipes = [];
+    $process = proc_open(
+        "git pull",
+        [
+            0 => ["pipe" , "r"], /* STDIN */
+            1 => ["pipe" , "w"], /* STDOUT */
+            2 => ["pipe" , "w"]  /* STDERR */
+        ],
+        $pipes
+    );
+    if (!$process) {
+        echo "failed to execute git pull\n";
+        exit(1);
+    }
+    sleep(3);
+    if (!fwrite($pipes[0] , $password)) {
+        echo "failed to pass password to git pull";
+        exit(1);
+    }
+    do {
+        $status = proc_get_status($process);
+        $out = fread($pipes[1] , 10);
+        $err = fread($pipes[2] , 10);
+        echo $out . $err;
+    } while ($status["running"] !== FALSE);
+    if ($status["exitcode"] !== 0) {
+        echo ERROR . "git pull terminated with exit code " . $status["exitcode"] . "\n";
+        return FALSE;
+    }
+
+    return TRUE;
+
+    /*
     $output = [];
 	if (exec("git pull" , $output , $exitcode) === FALSE) {
 		echo ERROR . "failed to execute git pull\n";
@@ -13,6 +46,8 @@ function git_pull():bool {
 		echo ERROR . "git pull terminated with exit code " . $exitcode . "\n";
         return FALSE;
 	}
+    */
+
     return TRUE;
 }
 
